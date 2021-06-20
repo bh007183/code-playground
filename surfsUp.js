d61c51de16544b78277effb32e35f29296273eec
Ben Hopkins <bjhops17@gmail.com>
2021-06-20 12:28:46 -0700

    continued playing with constructors and classes

--- surfsUp.js
+++ surfsUp.js
@@ -1,31 +1,51 @@
-class surftime {
-    constructor(penguin, chicken, other){
-        this.penguin = penguin
-        this.chicken = chicken
-        this.other = other
+class Surftime {
+  constructor(penguin, chicken, other) {
+    this.penguin = penguin;
+    this.chicken = chicken;
+    this.other = other;
+  }
+  getPenguin = () => {
+    if (this.penguin === "penguin") {
+      console.log("yes sir");
+    } else {
+      console.log("no way jose");
     }
-    getPenguin = () => {
-        if(this.penguin === "penguin"){
-            console.log("yes sir")
-        }else{console.log("no way jose")}
-    }
-
+  };
 }
 
+class Build extends Surftime {
+  constructor(penguin, chicken, other, name, value) {
+      super(penguin, chicken, other)
+      this.name = name
+      this.value = value;
 
-
-
-function Build(name, value){
-    this.name = name,
-    this.value = value
-
-    getName = () => {
-        console.log("getName")
-
-    }
-
+      this.getName =  () => {
+        console.log("getName");
+      };
+  }
+  
 }
+// function Build(penguin, chicken, other, name, value) {
+//   super(penguin, chicken, other);
+//   (this.name = name), (this.value = value);
 
-const ben = new Build("Ben", "None")
+//   getName = () => {
+//     console.log("getName");
+//   };
+// }
 
-console.log(ben)
+// Build.prototype = Object.create(Surftime.prototype);
+
+Build.prototype.constructor = Build;
+
+let Kevin = new Build(
+  "Emperor",
+  "Does not taste like",
+  "im surking my responsabilities",
+  "Kevin",
+  "100$"
+);
+
+// console.log(Kevin);
+console.log(Kevin.getPenguin())
+console.log(Kevin.getName())
